f700e82ee18e5adea7caa34a8bec12eeac587fd7
Pavel <rengetsu@mail.ru>
2022-01-20 22:32:57 +0200

    Create a 'Category' model

---
+++ GameShopWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace GameShopWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        //  We assign a default value for this property as current date and time
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
