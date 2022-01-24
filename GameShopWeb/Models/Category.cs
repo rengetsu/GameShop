7cce65625f2c3f24787f9c27936fc84ac3c63c64
Pavel <rengetsu@mail.ru>
2022-01-24 22:59:10 +0200

    Add data annotations to Category model

--- GameShopWeb/Models/Category.cs
+++ GameShopWeb/Models/Category.cs
@@ -1,11 +1,29 @@
-﻿namespace GameShopWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShopWeb.Models
 {
     public class Category
     {
+        /// <summary>
+        /// We make Id a primary key an identity column
+        /// </summary>
+        [Key]
         public int Id { get; set; }
+
+        /// <summary>
+        /// We make sure a Name is required property
+        /// </summary>
+        [Required]
         public string Name { get; set; }
+
+        /// <summary>
+        /// Display order
+        /// </summary>
         public int DisplayOrder { get; set; }
-        //  We assign a default value for this property as current date and time
+
+        /// <summary>
+        /// We assign a default value for this property as current date and time
+        /// </summary>
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
